837991235ea1faafc79bf069e1543ddcffd11f35
Srijan <srijan.kumar2021@gmail.com>
2024-01-11 22:10:20 +0530

    Frontend

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import SignOut from './pages/SignOut';
+import Profile from './pages/Profile';
+import About from './pages/About';
+import Navbar from './components/Navbar';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+   <Navbar/>
+   <Routes>
+    <Route path="/" element={<Home />}/>
+    <Route path="/sign-in" element={<SignIn />}/>
+    <Route path="/sign-up" element={<SignUp />}/>
+    <Route path="/sign-out" element={<SignOut />}/>
+    <Route path="/profile" element={<Profile />}/>
+    <Route path="/about" element={<About />}/>
+   </Routes>
+  </BrowserRouter>
+  )
+}
+
