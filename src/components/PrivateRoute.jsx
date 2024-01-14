7be9455c5ef9081e5c4f278a75f372f14458f371
Srijan <srijan.kumar2021@gmail.com>
2024-01-14 10:59:16 +0530

    complete profile page ui

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
-import { Outlet, Navaigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
 export default function PrivateRoute() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navaigate to="/sign-in" />;
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
 }
