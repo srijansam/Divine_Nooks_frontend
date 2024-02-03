7e0e60c18b19c47d79dae93a5ba227da24e76305
Srijan <srijan.kumar2021@gmail.com>
2024-02-04 01:26:49 +0530

    delete user

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -33,6 +33,18 @@ const userSlice = createSlice({
     updateUserFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
     }
   }
 });
@@ -42,7 +54,10 @@ export const {
   signInFailure,
   updateUserFailure,
   updateUserSuccess,
-  updateUserStart
+  updateUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+  deleteUserStart
 } = userSlice.actions;
 
 export default userSlice.reducer;
