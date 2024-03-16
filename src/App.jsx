2c2ceaf1e0d0ed307738a95ad7098fcb0e95d2ca
Srijan <srijan.kumar2021@gmail.com>
2024-03-17 00:18:11 +0530

    upto adding image slider

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import About from "./pages/About";
 import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 export default function App() {
   return (
@@ -18,11 +20,13 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
         </Route>
         <Route path="/about" element={<About />} />
+        <Route path="/update-listing/:listingId" element={<UpdateListing />} />
       </Routes>
     </BrowserRouter>
   );
